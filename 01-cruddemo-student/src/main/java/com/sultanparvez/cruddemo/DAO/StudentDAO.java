a82fb3411fd10524be87382695f98f0be5af64dd
sultanparvez <sultanparvez77.sp@gmail.com>
2023-08-07 00:03:38 +0600

    DAO Implementation

---
+++ 01-cruddemo-student/src/main/java/com/sultanparvez/cruddemo/DAO/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.sultanparvez.cruddemo.DAO;
+
+import com.sultanparvez.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void Save (Student theStudent);
+}
---
+++ 01-cruddemo-student/src/main/java/com/sultanparvez/cruddemo/DAO/StudentDAOImpl.java
@@ -0,0 +1,31 @@
+package com.sultanparvez.cruddemo.DAO;
+
+import com.sultanparvez.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+    //Define Field For entity manger
+    private EntityManager entityManger;
+
+    public StudentDAOImpl(EntityManager entityManger) {
+        this.entityManger = entityManger;
+    }
+
+    //Inject Entity Manager
+    @Autowired
+
+    //implement Save Method
+    @Override
+    @Transactional
+    public void Save(Student theStudent) {
+        entityManger.persist(theStudent);
+
+    }
+
+
+}
